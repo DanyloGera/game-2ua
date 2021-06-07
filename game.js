a32166157a3f0a86957a99a23721c2ebd4cbfbf4
DanyloGera <64353215+DanyloGera@users.noreply.github.com>
2021-06-07 12:35:56 +0300

    adaptive

--- game.js
+++ game.js
@@ -145,6 +145,10 @@ window.onload = function () {
 
   // before the game begins
   function preload() {
+    game.scale.scaleMode = Phaser.ScaleManager.USER_SCALE;
+    game.scale.setMinMax(700, 500, 900, 700);
+    game.scale.pageAlignHorizontally = true;
+    game.scale.pageAlignVertically = true;
     // game.stage.backgroundColor = '#5db1ad';
     game.load.image('background', 'background.png');
 
